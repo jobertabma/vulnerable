2d0d3eef619e9397242f91cc0901bb5042ee6b7f
Jobert Abma <jobertabma@gmail.com>
2015-03-24 16:12:40 +0100

    Added website attribute to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit :name, :bio
+    params.require(:user).permit :name, :bio, :website
   end
 
   def set_user
