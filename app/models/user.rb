b51414c6a0014613e6c8ff91b651519d1ef90047
Jobert Abma <jobertabma@gmail.com>
2015-03-24 16:44:57 +0100

    Added validation to the website attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+require 'uri'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :website, allow_blank: true, format: { with: URI::regexp(%w(http https)) }
 end
