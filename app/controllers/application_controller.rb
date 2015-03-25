a7d52a95df71fb089cbd06b6cfc5969a8b54fc32
Willian van der Velde <mail@willian.io>
2015-03-25 11:58:20 +0100

    Choose layout type by giving layout parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_layout
+
+  private
+
+  def set_layout
+    if ['small', 'wide'].include? params['layout']
+      cookies.permanent['layout'] = params['layout']
+    end
+
+    @layout = cookies.permanent['layout']
+  end
 end
